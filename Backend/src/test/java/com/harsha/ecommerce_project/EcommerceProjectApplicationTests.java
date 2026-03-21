e0211817a343f8354267c304a10ffff6a0153bf4
Harsha Dunna <dunnaharsha@gmail.com>
2026-03-21 21:44:04 +0530

    initialized spring boot backend project

---
+++ Backend/src/test/java/com/harsha/ecommerce_project/EcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsha.ecommerce_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
