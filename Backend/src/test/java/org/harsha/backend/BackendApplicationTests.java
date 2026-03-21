d2b1c2c1cd5487d30a8e8a2fa5f95f859707cfe4
Harsha Dunna <dunnaharsha@gmail.com>
2026-03-21 23:37:58 +0530

    setup backend spring boot project

--- Backend/src/test/java/org/harsha/backend/BackendApplicationTests.java
+++ Backend/src/test/java/org/harsha/backend/BackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.harsha.ecommerce_project;
+package org.harsha.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommerceProjectApplicationTests {
+class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
