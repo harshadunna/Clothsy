c9e697b5838da080b84ee397b29acfb2e2efbf08
Harsha Dunna <dunnaharsha@gmail.com>
2026-04-06 01:03:51 +0530

    configured Axios to use VITE_API_URL environment variable

--- Frontend/src/config/api.js
+++ Frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const LOCALHOST = "http://localhost:8080";
+const LOCALHOST = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 export const API_BASE_URL = LOCALHOST;
 
